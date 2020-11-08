a0f988a7ee6e280ce9128be0e33b9b343d461fbe
Nikita Ivanov <de1ay@nullteam.info>
2020-11-08 06:35:11 +0000

    Add: TradingTerminalCard layout

---
+++ src/components/trading_terminal/TradingTerminalCard.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div.card(:style="cardStyles")
+    div.card-header
+    div.card-body
+      slot
+</template>
+
+<script>
+export default {
+  name: 'TradingTerminalCard',
+  props: {
+    width: {
+      type: Number,
+      required: true,
+      validator: (val) => val > 0,
+    },
+    height: {
+      type: Number,
+      required: true,
+      validator: (val) => val > 0,
+    },
+  },
+  computed: {
+    cardStyles() {
+      return {
+        width: `${this.$props.width}px`,
+        height: `${this.$props.height}px`,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
